d15ef4fc6b67fe3cff5b52c5a0cc699ec29a8323
JoaoCardoso00 <jvcq13@gmail.com>
2025-12-07 22:57:30 -0300

    feat: better auth setup

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,6 +2,13 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ConvexReactClient } from "convex/react";
+import { authClient } from "@/lib/auth-client";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+  expectAuth: true,
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -12,7 +19,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      {children}
+      <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+        {children}
+      </ConvexBetterAuthProvider>
     </NextThemesProvider>
   )
 }
+
